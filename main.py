3f4de7be84636c2165f1b007e2621c02bac5b158
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-06-23 12:51:16 -0700

    feat: switch to logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from pprint import pprint
+import logging
+from pprint import pformat
 
 from args import parse_args
 from SnapchatMemoriesMetadataAdder.parser import parse_history
@@ -7,12 +8,14 @@ from SnapchatMemoriesMetadataAdder.parser import parse_history
 
 def main():
     args = parse_args()
+    # TODO: reduce logging level when done!
+    logging.basicConfig(level=logging.DEBUG)
+    
     with args.memories_history.open() as metadata:
         parsed = parse_history(json.load(metadata)["Saved Media"])
 
     for entry in parsed.items():
-        pprint(entry)
-
+        logging.debug("\n" + pformat(entry))
 
 if __name__ == "__main__":
     main()
