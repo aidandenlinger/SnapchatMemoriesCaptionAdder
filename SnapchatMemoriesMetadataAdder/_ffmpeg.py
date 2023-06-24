2fca383f6876dc8b744947e9adbdc4a91907f879
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-06-24 14:52:49 -0700

    feat/fix(ffmpeg): add metadata to all videos, fix sound not being copied

--- SnapchatMemoriesMetadataAdder/_ffmpeg.py
+++ SnapchatMemoriesMetadataAdder/_ffmpeg.py
@@ -16,17 +16,22 @@ def ffmpeg_add_metadata(base: Path, overlay: Optional[Path],
     NOTE: Only works on videos, does not work on images!"""
     assert metadata.type == MediaType.Video
 
+    vid = ffmpeg.input(str(base))
+    creation_time = f"creation_time={metadata.date.astimezone(tz).isoformat()}"
+
     if overlay:
-        base_vid = ffmpeg.input(str(base))
         overlay_img = ffmpeg.input(str(overlay))
         # Use scale2ref to scale the overlay to the video
-        scaled = ffmpeg.filter_multi_output([overlay_img, base_vid],
-                                            "scale2ref")
+        scaled = ffmpeg.filter_multi_output([overlay_img, vid], "scale2ref")
         # Overlay the overlay and save it to output!
-        creation_time = f"creation_time={metadata.date.astimezone(tz).isoformat()}"
-        final_cmd = scaled[1].overlay(scaled[0]).output(str(output),
-                                                        metadata=creation_time)
-        logging.debug(final_cmd.compile())
-        final_cmd.run(quiet=True)
+        overlay_video = scaled[1].overlay(scaled[0])
+        cmd = ffmpeg.output(
+            overlay_video,  # video
+            vid.audio,  # audio
+            str(output),  # output
+            metadata=creation_time)
     else:
-        copy(base, output)
+        cmd = vid.output(str(output), codec="copy", metadata=creation_time)
+
+    logging.debug(cmd.compile())
+    cmd.run(quiet=True)
