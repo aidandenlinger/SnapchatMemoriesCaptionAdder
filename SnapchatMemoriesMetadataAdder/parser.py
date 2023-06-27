c4f201277be3cfd5b994528e0e8d1c895b0cd502
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-06-27 16:14:06 -0700

    feat(parser): add logging for duplicate entries in memories_history.json

--- SnapchatMemoriesMetadataAdder/parser.py
+++ SnapchatMemoriesMetadataAdder/parser.py
@@ -1,3 +1,4 @@
+import logging
 from collections.abc import Collection, Mapping, Sequence
 from datetime import datetime
 from urllib.parse import parse_qs, urlparse
@@ -10,7 +11,7 @@ from SnapchatMemoriesMetadataAdder.metadata import MID, Location, MediaType, Met
 def parse_history(
         memory_history: Sequence[Mapping[str, str]]) -> Collection[Metadata]:
     """Parse memory metadata into python objects."""
-    parsed = set()
+    parsed: set[Metadata] = set()
 
     for entry in memory_history:
         mid: MID = parse_qs(urlparse(entry["Download Link"]).query)["mid"][0]
@@ -26,6 +27,23 @@ def parse_history(
                 "Latitude, Longitude: ").split(", ")
         ]
         location = Location(latitude, longitude)
+
+        data = Metadata(date, type, location, mid)
+
+        # memories_history.json seems to have duplicate entries. Their URLs
+        # differ (there's a different sid and sig in the url, whatever that is)
+        # but all of the data *we* parse with the same MID seem to have the same
+        # data. If someone *does* have an issue it should be loud
+        if mid in {p.mid for p in parsed}:
+            if data not in parsed:
+                logging.warning(
+                    f"Duplicate MID with different data: {mid} and "
+                    f"{[p for p in parsed if p.mid == mid]}")
+            else:
+                logging.info(
+                    f"{mid} has duplicates in memories_history.json but has "
+                    "same data, continuing...")
+                continue
 
         parsed.add(Metadata(date, type, location, mid))
 
