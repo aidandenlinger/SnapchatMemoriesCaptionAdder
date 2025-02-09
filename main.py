0f3926db3ca00feb26b8ea92a8f0f8a580b7a89e
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2025-02-09 13:35:04 -0800

    feat(main): default to _ffmpeg's error log as it has the info we need

--- main.py
+++ main.py
@@ -121,10 +121,8 @@ def main():
 
     print("Done!")
     if len(video_failures) != 0:
-        print("Some videos failed to convert:")
-        for vid in video_failures:
-            print(vid.mid)
-        print("Please try running the command with the `-vv` flag to see why this video failed to convert. Submit a Github issue with the output if you don't know why it failed to convert.")
+        print("Some videos failed to convert. See the error logs above.")
+        print("Please try running the command with the `-vv` flag to see why these videos failed to convert. Submit a Github issue with the output if you don't know why it failed to convert.")
 
 
 if __name__ == "__main__":
